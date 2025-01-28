eba8bfaa139f5631c1e183acace0b166a64fcf00
Cristian Contreras <cristian.d.contreras@gmail.com>
2025-01-28 15:34:41 -0300

    Agrega Funcionalidad

--- app.js
+++ app.js
@@ -1,39 +1,59 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 //Creo la lista
 let nombresAmigos=[];
-let nombre;
+let nombre="";
+let mensajeError = "Por favor, inserte un nombre";
+
 //Parte 2 Funcion agregar amigos
 function agregarAmigo(){
-    
-    nombre=document.getElementById("amigo").value;
-    
-    if(!document.getElementById("amigo").value=="")
-    {
-        if(verificarNombre(nombre)){
-            nombre = document.getElementById("amigo").value;
-            nombresAmigos.push(nombre);
-        }
-        
+//     //Forma 1
+//     //Capturar el valor
+//     nombre = document.getElementById("amigo").value;
+//     //Validad la entrada
+//     if(nombre===""){
+//         alert("Por favor, inserte un nombre");
+//     }
+//     //Actualizar array
+//     nombresAmigos.push(nombre);
+//     //limpiar entrada
+//     document.querySelector("#amigo").value="";
+
+
+// //forma 2
+//     if(document.getElementById("amigo").value===""){
+//         alert("Por favor, inserte un nombre");
+//     }else{
+//         nombresAmigos.push(document.getElementById("amigo").value);
+//         document.getElementById("amigo").value=="";
+//     }
+
+//forma 3
+    if(esCampoVacio("amigo")){
+        alert(mensajeError);
+        setFocusById("amigo");
     }else{
-        alert("Por favor, inserte un nombre");
+        nombre = getValueById("amigo");
+        nombresAmigos.push(nombre);
+        limpiarCampo("amigo");
+        setFocusById("amigo");
     }
-    
-    limpiarCampo("amigo");
 }
 
-//Validar entrada
-//Actualizar Array 
-function verificarNombre(nombre){
-    if(nombre===""){
-        alert("Por favor, inserte un nombre");
-    }else{
-        return typeof nombre === "string";
-    }
-    
+function getValueById(id){
+    return document.getElementById(id).value;
 }
 
+function setValueById(id, value){
+    document.getElementById(id).value=value;
+}
 
-//limpiar campo
-function limpiarCampo(inputID){
-    document.getElementById(inputID).value="";
+function setFocusById(id){
+    document.getElementById(id).focus();
+}
+
+function limpiarCampo(id){
+    setValueById(id, "");
+}
+function esCampoVacio(id){
+    return getValueById(id)==="";
 }
