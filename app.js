f578c78fa8da30297e61dfbf3a988b149dd44368
Cristian Contreras <cristian.d.contreras@gmail.com>
2025-01-27 23:51:22 -0300

    Actualiza el app.js

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+//Creo la lista
+let nombresAmigos=[];
+
+//Parte 2 Funcion agregar amigos
+function agregarAmigo(){
+    
+}
