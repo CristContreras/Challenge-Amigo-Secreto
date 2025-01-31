1c8e0852d6b16aba8e98ef23562b8c593352a75a
Cristian Contreras <cristian.d.contreras@gmail.com>
2025-01-31 15:37:42 -0300

    Refactoriza funciones

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
+//Funciones simplificadas
+const getElementById=id=>document.getElementById(id);
+const getValueById=id=>getElementById(id).value;
+const setValueById=(id, value)=>getElementById(id).value=value;
+const setFocusById = id => getElementById(id).focus();
+
+//Funciones para testear el nombre ingresado
+const contieneNumeros = nombre => /\d/.test(nombre);
+const esNombreVacio = nombre => nombre.trim() === "";
+const contieneCaracteresEspeciales = nombre => /[^A-Za-z\s]/.test(nombre);
+const contieneLetras = nombre => /^[A-Za-z\s]+$/.test(nombre.trim());
+
+
 let nombresAmigos = [];
 let nombre = "";
 let listaElement = getElementById("listaAmigos");
@@ -10,38 +23,40 @@ setFocusById(campoNombre);
 
 
 function agregarAmigo() {
-    nombre=getValueById(campoNombre);
-    if(esNombreValido(nombre)){
+    nombre = getValueById(campoNombre);
+    if (esNombreValido(nombre)) {
         nombresAmigos.push(nombre);
         limpiarCampo(campoNombre);
         actualizarLista(nombresAmigos);
     }
 }
-function reiniciarTodo(){
+function reiniciarTodo() {
     limpiarCampo(campoNombre);
     nombresAmigos.splice(0, nombresAmigos.length);
     listaElement.innerHTML = "";
-    resultadoElement.innerHTML="";
+    resultadoElement.innerHTML = "";
 }
 
 
-function contieneNumeros(nombre){
-    return /\d/.test(nombre);
-}
-function esNombreVacio(nombre){
-    return nombre.trim() === "";
-}
-function contieneCaracteresEspeciales(nombre){
-    return /[^A-Za-z\s]/.test(nombre);
-}
-function contieneLetras(nombre){
-    return /^[A-Za-z\s]+$/.test(nombre);
- }
-function esNombreValido(valorObtenido) {
+// function contieneNumeros(nombre) {
+//     return /\d/.test(nombre);
+// }
+
+// function esNombreVacio(nombre) {
+//     return nombre.trim() === "";
+// }
+// function contieneCaracteresEspeciales(nombre) {
+//     return /[^A-Za-z\s]/.test(nombre);
+// }
+// function contieneLetras(nombre) {
+//     return /^[A-Za-z\s]+$/.test(nombre.trim());
+// }
+
+
+function esNombreValido(nombre) {
 
     let mensajeError = contieneNumeros(nombre) ? "Error el nombre no debe contener números." : contieneCaracteresEspeciales(nombre) ? "Error debe contener solo letras." : esNombreVacio(nombre) ? "Error el nombre no puede ser vacío" : null;
     return contieneLetras(nombre) ? true : (alert(mensajeError), limpiarCampo(campoNombre), false);
-    
 }
 
 function actualizarLista(nombresAmigos) {
@@ -58,34 +73,41 @@ function sortearAmigo() {
         nombreSorteado = (nombresAmigos[numeroSorteado]).toUpperCase();
         resultadoElement.innerHTML = `El amigo secreto es ${nombreSorteado}`;
     } else {
-        alert("Debe ingresar al menos 2 nombres.")
+        alert("Debe ingresar al menos 2 nombres.");
+        limpiarCampo(campoNombre);
     }
 
 }
 
-function getRandomInt(max) {
-    return Math.floor(Math.random() * max);
-}
+//Funcion para generar un numero andom
+const getRandomInt = max => Math.floor(Math.random()*max);
+
+// function getRandomInt(max) {
+//     return Math.floor(Math.random() * max);
+// }
 
 //Permite utilizar la tecla Enter para agregar un nombre.
 document.getElementById(campoNombre).addEventListener("keydown", function (event) { if (event.key === "Enter") { agregarAmigo(); } })
 
 
-function getElementById(id) {
-    return document.getElementById(id);
-}
 
-function getValueById(id) {
-    return getElementById(id).value;
-}
+//  function getElementById(id) {
+//      return document.getElementById(id);
+//  }
 
-function setValueById(id, value) {
-    getElementById(id).value = value;
-}
+// function getValueById(id) {
+//     return getElementById(id).value;
+// }
 
-function setFocusById(id) {
-    getElementById(id).focus();
-}
+// function setValueById(id, value) {
+//     getElementById(id).value = value;
+// }
+
+// function setFocusById(id) {
+//      getElementById(id).focus();
+//  }
+
+
 
 function limpiarCampo(id) {
     setValueById(id, "");
